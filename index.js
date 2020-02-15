3dfc6b1787b55308c0e7610add2c930a6a89d5b6
patheticGeek <shivamnagi14@gmail.com>
2020-02-16 01:58:42 +0530

    v1

---
+++ index.js
@@ -0,0 +1,37 @@
+const axios = require("axios").default;
+const express = require("express");
+
+const dev = process.env.NODE_ENV !== "production";
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+
+const server = express();
+
+(async () => {
+  server.get("/", (req, res) => {
+    res.send("Send your query here: /pingback?link=https://example.com");
+  });
+
+  server.get("/ping", (req, res) => {
+    res.send(`Pong. Dev: ${dev} Port: ${PORT}`);
+  });
+
+  server.get("/pingback", async (req, res) => {
+    const link = req.query.link ? req.query.link.trim() : null;
+    let toSend;
+    if (!link || link === "") {
+      toSend = { error: true, message: "Link not provided" };
+    } else {
+      const resp = await axios.get(link);
+      toSend = {
+        error: false,
+        message: `URL: ${resp.config.url}, Status: ${resp.status}, Status Text: ${resp.statusText}`
+      };
+    }
+    console.log(toSend);
+    res.json(toSend);
+  });
+
+  server.listen(PORT, () => {
+    console.log(`> Running on http://localhost:${PORT}`);
+  });
+})();
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Ping Pong Bot
+
+This is a bot to ping a url passed by the api. You can run a preiodic ping to this api every 29.9 minutes to keep your heroku instance alive.
+
+If you want a 24h instance on heroku i suggest to have a different acc for each instance kept alive 24h as heroku free plan has 1000hrs/month limit. It means one 24h instance can be run for 41 days and two can be for 20.5 days.
+
+One-Click Deploy:
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/patheticGeek/ping-pong-bot)
