67946273eddb499c49c0738e01bfde898eda1d0b
patheticGeek <shivamnagi14@gmail.com>
2020-02-16 18:17:08 +0530

    try catch added

--- index.js
+++ index.js
@@ -8,19 +8,23 @@ const server = express();
 
 (async () => {
   server.get("/", async (req, res) => {
-    const link = req.query.link ? req.query.link.trim() : null;
-    let toSend;
-    if (!link || link === "") {
-      toSend = "Send your query here: /pingback?link=https://example.com";
-    } else {
-      const resp = await axios.get(link);
-      toSend = {
-        error: false,
-        message: `URL: ${resp.config.url}, Status: ${resp.status}, Status Text: ${resp.statusText}`
-      };
+    try {
+      const link = req.query.link ? req.query.link.trim() : null;
+      let toSend;
+      if (!link || link === "") {
+        toSend = "Send your query here: /pingback?link=https://example.com";
+      } else {
+        const resp = await axios.get(link);
+        toSend = {
+          error: false,
+          message: `URL: ${resp.config.url}, Status: ${resp.status}, Status Text: ${resp.statusText}`
+        };
+      }
+      console.log(toSend);
+      res.send(toSend);
+    } catch (e) {
+      res.send({ error: true, message: e.message ? e.message : "" });
     }
-    console.log(toSend);
-    res.send(toSend);
   });
 
   server.get("/ping", (req, res) => {
@@ -28,19 +32,23 @@ const server = express();
   });
 
   server.get("/pingback", async (req, res) => {
-    const link = req.query.link ? req.query.link.trim() : null;
-    let toSend;
-    if (!link || link === "") {
-      toSend = { error: true, message: "Link not provided" };
-    } else {
-      const resp = await axios.get(link);
-      toSend = {
-        error: false,
-        message: `URL: ${resp.config.url}, Status: ${resp.status}, Status Text: ${resp.statusText}`
-      };
+    try {
+      const link = req.query.link ? req.query.link.trim() : null;
+      let toSend;
+      if (!link || link === "") {
+        toSend = { error: true, message: "Link not provided" };
+      } else {
+        const resp = await axios.get(link);
+        toSend = {
+          error: false,
+          message: `URL: ${resp.config.url}, Status: ${resp.status}, Status Text: ${resp.statusText}`
+        };
+      }
+      console.log(toSend);
+      res.json(toSend);
+    } catch (e) {
+      res.send({ error: true, message: e.message ? e.message : "" });
     }
-    console.log(toSend);
-    res.json(toSend);
   });
 
   server.listen(PORT, () => {
